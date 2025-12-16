97f1ae55d64fb5cc1bfde13501ecd4b388488769
Maxim <kakashka921@gmail.com>
2025-12-16 12:12:15 +0100

    2.3

--- Assets/Scenes/Shooter.cs
+++ Assets/Scenes/Shooter.cs
@@ -1,14 +1,13 @@
-﻿using System.Runtime.CompilerServices;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public float bulletForce = 10f;
     public Transform firePoint;
 
     [SerializeField] private float lineSpeed = 10f;
     private LineRenderer _line;
+
     private bool _lineActive = false;
     private float _pressTimer = 0f;
 
@@ -17,7 +16,7 @@ public class Shooter : MonoBehaviour
         _line = GetComponent<LineRenderer>();
         if (_line == null)
         {
-            Debug.LogWarning("LineRenderer.");
+            Debug.LogWarning("LineRenderer не найден!");
             return;
         }
 
@@ -33,46 +32,42 @@ public class Shooter : MonoBehaviour
     void Update()
     {
         HandleShot();
-
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-        {
-            Shoot();
-        }
     }
 
-    void Shoot()
-    {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        if (rb != null)
-        {
-            rb.AddForce(firePoint.right * bulletForce);
-        }
-        else
-        {
-            Debug.LogWarning("У пули нет Rigidbody2D!");
-        }
-    }
     private void HandleShot()
     {
-        if (_line == null) return; 
+        if (_line == null) return;
 
         if (Input.GetMouseButtonDown(0))
         {
             _pressTimer = 0f;
             _lineActive = true;
         }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            _lineActive = false;
-            _line.SetPosition(1, Vector3.zero);
-        }
-
         if (_lineActive)
         {
             _pressTimer += Time.deltaTime;
             _line.SetPosition(1, Vector3.right * _pressTimer * lineSpeed);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            float shotPower = _pressTimer * lineSpeed;
+
+            Shoot(shotPower);
+
+            _lineActive = false;
+            _line.SetPosition(1, Vector3.zero);
+        }
+    }
+
+    void Shoot(float power)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.AddForce(firePoint.right * power, ForceMode2D.Impulse);
         }
     }
 }
